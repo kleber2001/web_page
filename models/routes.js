48fabb3d03c796d64e3f7981bfdd12826b3bd2e1
Kleber Nuñez <kgnunez@espol.edu.ec>
2023-01-25 11:27:24 -0500

    update mongo

--- models/routes.js
+++ models/routes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/router_db', {
-    useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/connection_ssh', {
+useNewUrlParser: true,
 },
 (err) => {
     if (!err) {
